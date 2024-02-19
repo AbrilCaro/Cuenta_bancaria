00dfd9f34ac9751994aac0b12a6351c279518c2c
AbrilCaro <abrilchr05@gmail.com>
2024-02-19 01:23:02 -0300

    ingreso y retiro

--- src/main/java/actividad_1/Cuenta.java
+++ src/main/java/actividad_1/Cuenta.java
@@ -1,2 +1,93 @@
-package actividad_1;public class Cuenta {
+package actividad_1;
+
+import java.util.Scanner;
+
+public class Cuenta {
+    private int numeroCuenta;
+    private long dni;
+    private double saldo;
+
+    //constructor
+
+    public Cuenta () {
+        this.numeroCuenta = 0;
+        this.dni = 0;
+        this.saldo = 0.0;
+    }
+
+    public Cuenta(int numeroCuenta, long dni, double saldo) {
+        this.numeroCuenta = numeroCuenta;
+        this.dni = dni;
+        this.saldo = saldo;
+    }
+
+    //get and set
+
+    public int getNumeroCuenta () {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta (int numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public long getDni () {
+        return dni;
+    }
+
+    public void setDni(long dni) {
+        this.dni = dni;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public static Cuenta crearCuenta () {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce el numero de cuenta");
+        int numeroCuenta = scanner.nextInt();
+        System.out.println("Introduce tu DNI");
+        long dni = scanner.nextLong();
+        System.out.println("Introduce tu saldo actual");
+        double saldo = scanner.nextDouble();
+
+        return new Cuenta(numeroCuenta, dni, saldo);
+    }
+
+    public void ingresar(double ingreso) {
+        saldo += ingreso;
+        System.out.println("Haz ingresado " + ingreso + " en tu cuenta.");
+    }
+
+    public void retirar(double retiro) {
+        if (saldo >= retiro) {
+            saldo -= retiro;
+            System.out.println("Haz retirado " + retiro + " de tu cuenta.");
+        } else {
+            saldo = 0;
+            System.out.println("Saldo insuficiente.");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Cuenta cuenta = Cuenta.crearCuenta();
+        System.out.println("Su cuenta ha sido creada con exito");
+        System.out.println("Numero de cuenta: " + cuenta.getNumeroCuenta());
+        System.out.println("Numero de DNI: " + cuenta.getDni());
+        System.out.println("Su saldo actual: " + cuenta.getSaldo());
+
+        cuenta.ingresar(500);
+        System.out.println("saldo despues del ingreso: $ " + cuenta.getSaldo());
+
+        cuenta.retirar(200);
+        System.out.println("Su saldo despues de retirar: $" + cuenta.getSaldo());
+
+
+    }
 }
--- src/main/java/actividad_1/Operacion.java
+++ src/main/java/actividad_1/Operacion.java
@@ -1,2 +1,87 @@
-package actividad_1;public class Operacion {
+package actividad_1;
+
+import java.util.Scanner;
+
+public class Operacion {
+    private double numero1;
+    private double numero2;
+
+
+    public Operacion(double numero1, double numero2) {
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+    }
+
+    public Operacion() {
+        this.numero1 = 0.0;
+        this.numero2 = 0.0;
+    }
+
+    public double getNumero1() {
+        return numero1;
+    }
+
+    public void setNumero1(double numero1) {
+        this.numero1 = numero1;
+    }
+
+    public double getNumero2() {
+        return numero2;
+    }
+
+    public void setNumero2(double numero2) {
+        this.numero2 = numero2;
+    }
+
+    public void crearOperacion() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce el primer numero:");
+        this.numero1 = scanner.nextDouble();
+        System.out.println("Introduce el segundo numero:");
+        this.numero2 = scanner.nextDouble();
+    }
+
+    //suma
+    public double suma() {
+        return numero1 + numero2;
+    }
+
+    //resta
+
+    public double resta() {
+        return numero1 - numero2;
+    }
+
+    //multiplicacion
+
+    public double multiplicar() {
+
+        if (numero1 == 0||numero2 == 0) {
+        System.out.println("No se puede multiplicar por cero");
+        return 0; }
+          else {
+        return numero1 * numero2;
+    }
+
+    }
+
+//division
+
+    public double division () {
+        if (numero1 == 0 || numero2 == 0) {
+            System.out.println("No se puede dividir por cero");
+            return 0; }
+        else {
+            return numero1 / numero2;
+        }
+    }
+
+    public static void main(String[] args) {
+        Operacion operacion = new Operacion();
+        operacion.crearOperacion();
+        System.out.println("Suma: " + operacion.suma());
+        System.out.println("Resta: " + operacion.resta());
+        System.out.println("Multiplicacion: " + operacion.multiplicar());
+        System.out.println("Division: " + operacion.division());
+    }
 }
